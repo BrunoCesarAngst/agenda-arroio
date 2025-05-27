396988daaa42f282f528655d9774b5358ca0704f
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2025-05-27 01:04:50 -0300

    feat: remove componentes e lógica desnecessários da página Home, simplificando a estrutura e adicionando uma mensagem de teste de deploy

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import { inject } from '@vercel/analytics';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index';
-import './services/firebase';
-import './style.css';
-inject()
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import './style.css'
 
 const app = createApp(App)
 app.use(router)
